795eada1a9229feeeae66b75f405769d8d3aa381
AlexanderGreyFox <alexserov0@gmail.com>
2022-06-23 16:07:29 +0300

    Make it more scalable

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # ar_script
 
-A script for obtaining currency exchange rate data and plotting based on this data
+A script that receives data on exchange rates for the last 30 days and builds a graph based on them.
+
+For starting you need to setup requirements from requirements.txt file (python3 -m pip install -r requirements.txt)
--- script.py
+++ script.py
@@ -1,7 +1,6 @@
-import aiohttp
-import asyncio
 import datetime
 import pylab
+import requests
 import matplotlib.pyplot as plt
 
 from matplotlib.backends.backend_pdf import PdfPages
@@ -10,45 +9,49 @@ from statistics import mean
 
 base_url = 'https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1'
 
+ex = {
+        'usd': 'rub',
+        'gel': 'rub'
+      }
 
-async def api_request(date: datetime.date, first_currency: str, second_currency: str) -> float:
+
+def api_request(date: datetime.date, first_currency: str, second_currency: str) -> float:
     url = f'{base_url}/{date}/currencies/{first_currency}/{second_currency}.json'
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url) as resp:
-            result = await resp.json()
-            return result.get(second_currency)
+    resp = requests.get(url)
+    return resp.json().get(second_currency)
 
 
-async def main():
-    pdf = PdfPages("Exchange_rate.pdf")
+def main():
+    pdf = PdfPages("Exchange_rate_example.pdf")
     base = datetime.datetime.date(datetime.datetime.today())
     date_list = [base - datetime.timedelta(days=x) for x in range(30)]
-    usd_list = []
-    gel_list = []
+    ex_dict = {}
 
-    for date in date_list:
-        usd_resp = await api_request(date, 'usd', 'rub')
-        gel_resp = await api_request(date, 'gel', 'rub')
-        usd_list.append(usd_resp)
-        gel_list.append(gel_resp)
+    for k, v in ex.items():
+        for date in date_list:
+            resp = api_request(date, k, v)
+            if k in ex_dict:
+                ex_dict[k].append(resp)
+            else:
+                ex_dict[k] = [resp]
 
     plt.figure(figsize=(20, 10))
 
-    pylab.subplot(2, 1, 1)
-    plt.title('USD/RUB')
-    plt.plot(date_list, usd_list, label=f'min={min(usd_list)}\nmax={max(usd_list)}\nmean={mean(usd_list)}')
-    plt.legend()
-
-    pylab.subplot(2, 1, 2)
-    plt.title('GEL/RUB')
-    plt.plot(date_list, gel_list, label=f'min={min(gel_list)}\nmax={max(gel_list)}\nmean={mean(gel_list)}')
-    plt.legend()
+    size = len(ex_dict)
+    pos = 1
+    for k, v in ex_dict.items():
+        pylab.subplot(size, 1, pos)
+        plt.title(f'{k} exchange rate')
+        plt.xlabel("date")
+        plt.ylabel("rub")
+        plt.plot(date_list, v, label=f'min={min(v)}\nmax={max(v)}\nmean={mean(v)}')
+        plt.legend()
+        pos += 1
 
     pdf.savefig()
     plt.close()
     pdf.close()
 
+
 if __name__ == '__main__':
-    asyncio.run(main())
-
-
+    main()
