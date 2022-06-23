7879284582ec70d060ade93c0a5ea6eafe77903e
AlexanderGreyFox <alexserov0@gmail.com>
2022-06-23 16:28:30 +0300

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+import os
+
+from setuptools import setup
+
+lib_folder = os.path.dirname(os.path.realpath(__file__))
+requirement_path = lib_folder + '/requirements.txt'
+install_requires = []
+if os.path.isfile(requirement_path):
+    with open(requirement_path) as f:
+        install_requires = f.read().splitlines()
+setup(
+    name='Exchange_script',
+    version='0.1.0',
+    author='Serov Aleksandr',
+    author_email='alexserov0@gmail.com',
+    packages=['script'],
+    description='A script that receives data on exchange rates for the last 30 days and builds a graph based on them.',
+    long_description=open('README.md').read(),
+    install_requires=install_requires,
+)
